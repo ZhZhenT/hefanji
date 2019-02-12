9213b4623d242b95024865106f028a259cff448b
ZhanZhenT <849723358@qq.com>
2019-02-12 23:17:32 +0800

    '1'

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
 //app.js
 const utils = require('./utils/util.js')
 App({
-  onLaunch: function () { // 当小程序初始化完成时，会触发 onLaunch（全局只触发一次） console.log('App onLaunch')
+  onLaunch: function() { // 当小程序初始化完成时，会触发 onLaunch（全局只触发一次） console.log('App onLaunch')
     var that = this;
-    
-    that.getUserToken = function (userid){
-      return new Promise(function (resolve, reject) {
+
+    that.getUserToken = function(userid) {
+      return new Promise(function(resolve, reject) {
         // if (that.globalData.tokenRes){
         //   resolve(that.globalData.tokenRes)
         //   return 
         // }
-
         wx.login({
           success: res => {
             //发送 res.code 到后台换取 openId, sessionKey, unionId
-            utils.request('http://fanmofang.17d3.com/api/user/login/wechat', { data:
-               {
-                code: res.code,
-                referer_user_id: userid || ''
-               }
+
+            utils.request('http://fanmofang.17d3.com/api/user/login/wechat', {
+                data: {
+                  code: res.code,
+                  referer_user_id: userid || ''
+                }
               })
-              .then(function (res) {
+              .then(function(res) {
                 console.log(res, '登陆信息')
-                that.globalData.tokenRes = res 
+                that.globalData.tokenRes = res
                 that.globalData.userid = res.data.user_info.id
                 wx.setStorageSync('mobile', res.data.user_info.mobile)
                 resolve(res)
-              }, function (err) {
+              }, function(err) {
+
                 reject(err)
               })
           }
@@ -35,7 +36,7 @@ App({
     }
 
   },
-  onShow:function(){
+  onShow: function() {
 
   },
   // 全局变量 
